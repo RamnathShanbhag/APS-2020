fe4189dc51882eeb98b5e91d2328e19ac61d73ae
RamnathShanbhag <60292057+RamnathShanbhag@users.noreply.github.com>
2020-03-21 16:27:54 +0530

    Add files via upload

---
+++ josephus_non_recursive.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int josephus(int n,int k)
+{
+	int res=0;
+	for(int i=1;i<=n;++i)
+	{
+		res=(res+k)%i;
+	}
+	return res+1;
+}
+int main()
+{
+	int n,k;
+	cin>>n>>k;
+	int result;
+	result=josephus(n,k);
+	cout<<result<<"\n";
+}
